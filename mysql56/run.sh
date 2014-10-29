d7612c4df3ec61aa93220e847eae62a1c1e38a8d
kolosov <magic2ktech@gmail.com>
2014-10-29 22:19:10 +0300

    another day, another edit

--- mysql56/run.sh
+++ mysql56/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 /etc/init.d/mysql start
 
-mysql -uroot -e 'create database bbmobile_admin CHARACTER SET utf8 COLLATE utf8_general_ci;'
-mysql -uroot -e 'create database bbmobile_users CHARACTER SET utf8 COLLATE utf8_general_ci;'
-mysql -uroot -e 'create database bbmobile_commons CHARACTER SET utf8 COLLATE utf8_general_ci;'
-mysql -uroot -e 'create database bbmobile_const CHARACTER SET utf8 COLLATE utf8_general_ci;'
-mysql -uroot -e 'create database bbmobile_replays CHARACTER SET utf8 COLLATE utf8_general_ci;'
-mysql -uroot -e 'create database bbmobile_logs CHARACTER SET utf8 COLLATE utf8_general_ci;'
+mysql -uroot -e "create database bbmobile_admin CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "create database bbmobile_users CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "create database bbmobile_commons CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "create database bbmobile_const CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "create database bbmobile_replays CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "create database bbmobile_logs CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-mysql -uroot -e 'grant all privileges on *.* to bbmobiledev@'%' identified by '123';'
+mysql -uroot -e "grant all privileges on *.* to bbmobiledev@'%' identified by 'passwd';"
 
 
 /etc/init.d/mysql restart
