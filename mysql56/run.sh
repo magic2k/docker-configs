9a90f67973b4947fe96abd9636c496a088b95fe2
magic2k <magic2ktech@gmail.com>
2014-11-17 17:05:26 +0300

    mysql56 changes

--- mysql56/run.sh
+++ mysql56/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+apt-get install -y mysql-server-5.6 && rm -rf /var/lib/apt/lists/*
+sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
 /etc/init.d/mysql start
+
+# some time to let mysqld start
+sleep 8;
 
 mysql -uroot -e "create database bbmobile_admin CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -e "create database bbmobile_users CHARACTER SET utf8 COLLATE utf8_general_ci;"
