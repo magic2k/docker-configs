bb79e17b988e7f335c71d9ef53c73dd0f3771d1d
magic2k <magic2ktech@gmail.com>
2015-01-13 19:28:19 +0300

    moo

--- excercise_docker/start.sh
+++ excercise_docker/start.sh
@@ -15,7 +15,12 @@ cd ..
 #run them
 docker run -d -h redis_primary --name redis_primary jamtur01/redis_primary
 docker run -d -h redis_replica1 --name redis_replica1 --link redis_primary:redis_primary jamtur01/redis_replica
+docker run -d -h redis_replica2 --name redis_replica2 --link redis_primary:redis_primary jamtur01/redis_replica
+docker run -d --name nodeapp -p 3000:3000 --link redis_primary:redis_primary jamtur01/nodejs
 
 #show logs
 docker run --rm -ti --volumes-from redis_primary ubuntu cat /var/log/redis/redis-server.log
 docker run --rm -ti --volumes-from redis_replica1 ubuntu cat /var/log/redis/redis-replica.log
+docker run --rm -ti --volumes-from redis_replica2 ubuntu cat /var/log/redis/redis-replica.log
+
+
